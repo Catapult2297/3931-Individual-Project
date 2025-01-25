2cac69c6244fa82b22f7dcee9550c4655b5262b7
Catapult2297 <wbhh9hdu@duck.com>
2025-01-25 22:11:33 +0000

    Added functions and some documentations to hoare triple

--- hoare_logic/src/bin/hoare_triple.rs
+++ hoare_logic/src/bin/hoare_triple.rs
@@ -1,7 +1,21 @@
+//! # Hoare Logic implementation in Rust
+//!
+//! This module provides an implementation of Hoare Logic Triple using an struct `Triple`.
+//! It supports the following Hoare Logic axioms and rules:
+//! - Empty Statement Axiom
+//! - Assignment Axiom
+//! - Rule of Composition
+//! - Condition Rule
+//! - Consequence Rule
+//! - While Rule
+
 #[allow(dead_code)]
 use std::fmt;
+
+use first_order::Formula;
 mod first_order;
 
+/// A struct for storing the three parts of a Hoare Triple
 pub struct Triple {
     precondition: first_order::Formula,
     command: String,
@@ -9,6 +23,21 @@ pub struct Triple {
 }
 
 impl Triple {
+    /// Creates a new `Triple` from three string input.
+    ///
+    /// # Arguments
+    /// * `precondition` - A `String` or `&str` that represents the logical formula in prefix notation. Every atomic propositions, logical connectives, and logical quantifiers must be separated using a whitespace.
+    /// * `command` - A `String` or `&str` that represents the command of the Triple.
+    /// * `precondition` - A `String` or `&str` that represents the logical formula in prefix notation. Every atomic propositions, logical connectives, and logical quantifiers must be separated using a whitespace.
+    ///
+    /// # Returns
+    /// A `Triple` instance representing the parsed input.
+    ///
+    /// # Example
+    /// ```
+    /// let triple1: Triple = Triple::new("= ⊤ ⊤", "x≔5", "= x 5");
+    /// println!("{triple1}"); // Output: {(⊤=⊤)} x≔5 {(x=5)}
+    /// ```
     pub fn new<T: Into<String>>(precondition: T, command: T, postcondition: T) -> Triple {
         Triple {
             precondition: first_order::Formula::new(precondition),
@@ -27,15 +56,96 @@ impl fmt::Display for Triple {
         )
     }
 }
+/// Creates a new `Triple` using the Rule of Composition [1].
+///
+/// # Arguments
+/// * `left` - The Triple executed first.
+/// * `right` - The Triple executed after `left`.
+///
+/// # Returns
+/// A `Triple` instance with the Rule of Composition applied on `left` and `right`.
+///
+/// # Example
+/// ```
+/// let triple1: Triple = Triple::new("= ⊤ ⊤", "x≔5", "= x 5");
+/// let triple2: Triple = Triple::new("= x 5", "y≔x+1", "= y 6");
+/// let triple3: Triple = composition_rule(&triple1, &triple2);
+/// println!("{triple3}"); // Output: {(⊤=⊤)} x≔5;y≔x+1 {(y=6)}
+/// ```
+/// [1]: https://en.wikipedia.org/wiki/Hoare_logic#Rule_of_composition
+pub fn composition_rule(left: &Triple, right: &Triple) -> Triple {
+    Triple::new(
+        left.precondition.to_string(),
+        format!("{}{}{}", left.command, ";", right.command),
+        right.postcondition.to_string(),
+    )
+}
+
+/// Creates a new `Triple` using the Condition Rule [1].
+///
+/// # Arguments
+/// * `left` - The Triple with the unnegated condition.
+/// * `right` - The Triple with the negated condition.
+///
+/// # Returns
+/// A `Triple` instance with the Condition Rule applied on `left` and `right`.
+///
+/// # Example
+/// ```
+/// let triple1: Triple = Triple::new("= ⊤ ⊤", "x≔5", "= x 5");
+/// let triple2: Triple = Triple::new("= x 5", "y≔x+1", "= y 6");
+/// let triple3: Triple = composition_rule(&triple1, &triple2);
+/// println!("{triple3}"); // Output: {(⊤=⊤)} x≔5;y≔x+1 {(y=6)}
+/// ```
+/// [1]: https://en.wikipedia.org/wiki/Hoare_logic#Rule_of_composition
+pub fn condition_rule(left: &Triple, right: &Triple) -> Triple {
+    Triple::new(
+        format!("{}", left.precondition.get_info()[2]),
+        format!(
+            "if {} then {} else {} endif",
+            left.precondition.get_info()[1],
+            left.command,
+            right.command,
+        ),
+        left.postcondition.to_string(),
+    )
+}
+
+pub fn consequence_rule(left: &Formula, middle: &Triple, right: &Formula) -> Triple {
+    Triple::new(
+        format!("{}", left.get_info()[2]),
+        format!("{}", middle.command),
+        format!("{}", right.get_info()[2]),
+    )
+}
+
+pub fn while_rule(input: &Triple) -> Triple {
+    Triple::new(
+        input.postcondition.to_string(),
+        format!(
+            " while {} do {} done",
+            input.precondition.get_info()[2],
+            input.command
+        ),
+        format!(
+            "∧ ¬ {} {}",
+            input.precondition.get_info()[2],
+            input.postcondition.to_string()
+        ),
+    )
+}
 
 fn main() {
     let test: Triple = Triple {
-        precondition: first_order::Formula::new("⊤"),
-        command: String::from("a := 5"),
+        precondition: first_order::Formula::new("= ⊤ ⊤"),
+        command: String::from("a ≔ 5"),
         postcondition: first_order::Formula::new("= a 5"),
     };
 
-    let test2: Triple = Triple::new("⊤", "skip", "⊤");
+    let test2: Triple = Triple::new("= ⊤ ⊤", "skip", "= ⊤ ⊤");
+    println!("{}", composition_rule(&test, &test2));
+    println!("{}", condition_rule(&test, &test2));
+    println!("{}", while_rule(&test));
     println!("{test}");
-    println!("{test2}")
+    println!("{test2}");
 }
