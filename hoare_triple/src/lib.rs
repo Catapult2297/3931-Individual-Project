1329b8553225556e3a286f1cfc0c0ea5021e67ba
Catapult2297 <wbhh9hdu@duck.com>
2025-04-10 22:05:26 +0100

    Added tests for `hoare_triple`.

--- hoare_triple/src/lib.rs
+++ hoare_triple/src/lib.rs
@@ -185,9 +185,9 @@ pub fn condition_rule(left: &Triple, right: &Triple) -> Result<Triple, String> {
             left.precondition.get_info()[1],
             negated_condition
         ));
-    } else if left.postcondition.to_string() != right.postcondition.to_string() {
+    } else if left.postcondition != right.postcondition {
         return Err(format!(
-            "The input triples do not have identical postconditions\nleft: {:?}, right: {:?}",
+            "The input triples do not have identical postconditions\nleft: {}, right: {}",
             left.postcondition, right.postcondition
         ));
     }
@@ -250,13 +250,13 @@ pub fn consequence_rule(
     } else if left.get_info()[2] != middle.precondition.to_prefix_notation() {
         return Err(format!(
             "The left `Formula` {:?} does not match the precondition of the middle `Triple` {:?}",
-            left.get_info()[2],
+            left.to_prefix_notation(),
             middle.precondition.to_prefix_notation()
         ));
     } else if right.get_info()[1] != middle.postcondition.to_prefix_notation() {
         return Err(format!(
             "The right `Formula` {:?} does not match the postcondition of the middle `Triple` {:?}",
-            right.get_info()[1],
+            right.to_prefix_notation(),
             middle.postcondition.to_prefix_notation()
         ));
     }
@@ -266,6 +266,7 @@ pub fn consequence_rule(
         format!("{}", right.get_info()[2]),
     ))
 }
+
 /// Creates a new `Triple` using the While Rule [4].
 ///
 /// # Arguments
@@ -288,7 +289,7 @@ pub fn consequence_rule(
 pub fn while_rule(input: &Triple) -> Result<Triple, String> {
     if input.precondition.get_info()[1] != input.postcondition.to_prefix_notation() {
         return Err(format!(
-            "The loop invariant is not preserved\nprecondition (P∧B): {}, postcondition (P): {}",
+            "The loop invariant is not preserved\nprecondition (P∧B): {:?}, postcondition (P): {:?}",
             Formula::new(&input.precondition.get_info()[1]).to_prefix_notation(),
             input.postcondition.to_prefix_notation()
         ));
@@ -306,4 +307,295 @@ pub fn while_rule(input: &Triple) -> Result<Triple, String> {
             input.postcondition.to_prefix_notation()
         ),
     ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use first_order::Formula;
+    use std::panic;
+
+    #[test]
+    fn test_basic_valid_input() {
+        let test_triple = Triple::new("A", "B", "C");
+        let expected_triple = Triple {
+            precondition: Formula::new("A"),
+            command: "B".to_string(),
+            postcondition: Formula::new("C"),
+        };
+        assert_eq!(test_triple, expected_triple);
+    }
+
+    #[test]
+    fn test_empty_strings() {
+        let result = panic::catch_unwind(|| {
+            Triple::new("", "", "");
+        });
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_whitespace_handling() {
+        let result = panic::catch_unwind(|| {
+            Triple::new("   ", "   ", "   ");
+        });
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_special_characters() {
+        let test_triple = Triple::new("∧ ∀ x x y", "x≔z", "∧ ∀ z z y");
+        let expected_triple = Triple {
+            precondition: Formula::new("∧ ∀ x x y"),
+            command: "x≔z".to_string(),
+            postcondition: Formula::new("∧ ∀ z z y"),
+        };
+        assert_eq!(test_triple, expected_triple);
+    }
+
+    #[test]
+    fn test_long_strings() {
+        let long_precondition = "A".repeat(1000);
+        let long_command = "B".repeat(1000);
+        let long_postcondition = "C".repeat(1000);
+        let test_triple = Triple::new(&long_precondition, &long_command, &long_postcondition);
+        let expected_triple = Triple {
+            precondition: Formula::new(&long_precondition),
+            command: long_command.clone(),
+            postcondition: Formula::new(&long_postcondition),
+        };
+        assert_eq!(test_triple, expected_triple);
+    }
+
+    #[test]
+    fn test_identical_pre_post_conditions() {
+        let test_triple = Triple::new("X", "Y", "X");
+        let expected_triple = Triple {
+            precondition: Formula::new("X"),
+            command: "Y".to_string(),
+            postcondition: Formula::new("X"),
+        };
+        assert_eq!(test_triple, expected_triple);
+    }
+
+    #[test]
+    fn test_complex_formulas() {
+        let test_triple = Triple::new(
+            "∧ ∀ x → P(x) ∧ Q(x) ∃ y ∨ R(y) S(y) = ¬ T(x) < U V",
+            "x≔z",
+            "∧ ∀ z → P(z) ∧ Q(z) ∃ y ∨ R(y) S(y) = ¬ T(z) < U V",
+        );
+        let expected_triple = Triple {
+            precondition: Formula::new("∧ ∀ x → P(x) ∧ Q(x) ∃ y ∨ R(y) S(y) = ¬ T(x) < U V"),
+            command: "x≔z".to_string(),
+            postcondition: Formula::new("∧ ∀ z → P(z) ∧ Q(z) ∃ y ∨ R(y) S(y) = ¬ T(z) < U V"),
+        };
+        assert_eq!(test_triple, expected_triple);
+    }
+
+    #[test]
+    fn consequence_test() {
+        let triple1: Triple = Triple::new("= x+1 43", "y≔x+1", "= y 43");
+        let triple2: Triple = Triple::new("= y 43", "z≔y", "= z 43");
+        let test_triple: Triple = composition_rule(&triple1, &triple2).unwrap();
+        let result: Triple = Triple::new("= x+1 43", "y≔x+1;z≔y", "= z 43");
+        assert_eq!(test_triple, result);
+    }
+
+    #[test]
+    fn consequence_multiple_triples() {
+        let triple1: Triple = Triple::new("= x+1 43", "y≔x+1", "= y 43");
+        let triple2: Triple = Triple::new("= y 43", "z≔y", "= z 43");
+        let triple3: Triple = Triple::new("= z 43", "w≔z", "= w 43");
+
+        let intermediate: Triple = composition_rule(&triple1, &triple2).unwrap();
+        let final_triple: Triple = composition_rule(&intermediate, &triple3).unwrap();
+
+        let expected: Triple = Triple::new("= x+1 43", "y≔x+1;z≔y;w≔z", "= w 43");
+        assert_eq!(final_triple, expected);
+    }
+
+    #[test]
+    fn consequence_no_composition() {
+        let triple1: Triple = Triple::new("= x+1 43", "y≔x+1", "= y 43");
+        let triple2: Triple = Triple::new("= z 44", "w≔z", "= w 44");
+
+        let result = composition_rule(&triple1, &triple2);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn consequence_identity_triple() {
+        let triple1: Triple = Triple::new("= x+1 43", "y≔x+1", "= y 43");
+        let identity: Triple = Triple::new("= y 43", "y≔y", "= y 43");
+
+        let test_triple: Triple = composition_rule(&triple1, &identity).unwrap();
+        let expected: Triple = Triple::new("= x+1 43", "y≔x+1;y≔y", "= y 43");
+        assert_eq!(test_triple, expected);
+    }
+
+    #[test]
+    fn consequence_complex_formulas() {
+        let triple1: Triple = Triple::new("= 2*x + 1 43", "y≔2*x+1", "= y 43");
+        let triple2: Triple = Triple::new("= y 43", "z≔y", "= z 43");
+
+        let test_triple: Triple = composition_rule(&triple1, &triple2).unwrap();
+        let expected: Triple = Triple::new("= 2*x + 1 43", "y≔2*x+1;z≔y", "= z 43");
+        assert_eq!(test_triple, expected);
+    }
+
+    #[test]
+    fn test_condition_rule_valid() {
+        let triple1 = Triple::new("∧ B P", "S", "Q");
+        let triple2 = Triple::new("∧ ¬ B P", "T", "Q");
+        let result = condition_rule(&triple1, &triple2).unwrap();
+        let expected = Triple::new("P", "if B then S else T endif", "Q");
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_condition_rule_invalid_conjunction() {
+        let triple1 = Triple::new("P", "S", "Q"); // Not a conjunction
+        let triple2 = Triple::new("∧ ¬ B P", "T", "Q");
+        let result = condition_rule(&triple1, &triple2);
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "The input triples do not have `Conjunction` formulae as precondition".to_string()
+        );
+    }
+
+    #[test]
+    fn test_condition_rule_mismatched_conditions() {
+        let triple1 = Triple::new("∧ B P", "S", "Q");
+        let triple2 = Triple::new("∧ ¬ C P", "T", "Q"); // Mismatched negated condition
+        let result = condition_rule(&triple1, &triple2);
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "The input triples do not match negated \"B\" and unnegated \"C\" conditions"
+                .to_string()
+        );
+    }
+
+    #[test]
+    fn test_condition_rule_different_postconditions() {
+        let triple1 = Triple::new("∧ B P", "S", "Q1");
+        let triple2 = Triple::new("∧ ¬ B P", "T", "Q2"); // Different postconditions
+        let result = condition_rule(&triple1, &triple2);
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "The input triples do not have identical postconditions\nleft: Q1, right: Q2"
+                .to_string()
+        );
+    }
+
+    #[test]
+    fn test_condition_rule_empty_conditions() {
+        let triple1 = Triple::new("∧ B P", "S", "Q");
+        let triple2 = Triple::new("∧ ¬ B P", "", "Q"); // Empty command in right triple
+        let result = condition_rule(&triple1, &triple2).unwrap();
+        let expected = Triple::new("P", "if B then S else  endif", "Q");
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_consequence_rule_valid() {
+        let formula1 = Formula::new("→ P1 P2");
+        let formula2 = Formula::new("→ Q2 Q1");
+        let triple1 = Triple::new("P2", "S", "Q2");
+        let result = consequence_rule(&formula1, &triple1, &formula2).unwrap();
+        let expected = Triple::new("P1", "S", "Q1");
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_consequence_rule_invalid_left_formula() {
+        let formula1 = Formula::new("P1"); // Not an implication
+        let formula2 = Formula::new("→ Q2 Q1");
+        let triple1 = Triple::new("P2", "S", "Q2");
+        let result = consequence_rule(&formula1, &triple1, &formula2);
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "The left `Formula` \"P1\" is not an Implication type Formula. Left type: \"Term\""
+                .to_string()
+        );
+    }
+
+    #[test]
+    fn test_consequence_rule_invalid_right_formula() {
+        let formula1 = Formula::new("→ P1 P2");
+        let formula2 = Formula::new("Q1"); // Not an implication
+        let triple1 = Triple::new("P2", "S", "Q2");
+        let result = consequence_rule(&formula1, &triple1, &formula2);
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "The right `Formula` \"Q1\" is not an Implication type Formula. Right type: \"Term\""
+                .to_string()
+        );
+    }
+
+    #[test]
+    fn test_consequence_rule_mismatched_precondition() {
+        let formula1 = Formula::new("→ P1 P2");
+        let formula2 = Formula::new("→ Q2 Q1");
+        let triple1 = Triple::new("P3", "S", "Q2"); // Mismatched precondition
+        let result = consequence_rule(&formula1, &triple1, &formula2);
+        assert!(result.is_err());
+        assert_eq!(result.err().unwrap(), "The left `Formula` \"→ P1 P2\" does not match the precondition of the middle `Triple` \"P3\"".to_string());
+    }
+
+    #[test]
+    fn test_consequence_rule_mismatched_postcondition() {
+        let formula1 = Formula::new("→ P1 P2");
+        let formula2 = Formula::new("→ Q2 Q1");
+        let triple1 = Triple::new("P2", "S", "Q3"); // Mismatched postcondition
+        let result = consequence_rule(&formula1, &triple1, &formula2);
+        assert!(result.is_err());
+        assert_eq!(result.err().unwrap(), "The right `Formula` \"→ Q2 Q1\" does not match the postcondition of the middle `Triple` \"Q3\"".to_string());
+    }
+
+    #[test]
+    fn test_while_rule_valid() {
+        let triple1 = Triple::new("∧ P B", "S", "P");
+        let result = while_rule(&triple1).unwrap();
+        let expected = Triple::new("P", "while B do S done", "∧ ¬ B P");
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_while_rule_invalid_invariant() {
+        let triple1 = Triple::new("∧ P B", "S", "Q"); // Postcondition does not match invariant
+        let result = while_rule(&triple1);
+        assert!(result.is_err());
+        assert_eq!(result.err().unwrap(), "The loop invariant is not preserved\nprecondition (P∧B): \"P\", postcondition (P): \"Q\"".to_string());
+    }
+
+    #[test]
+    fn test_while_rule_empty_command() {
+        let triple1 = Triple::new("∧ P B", "", "P"); // Empty command
+        let result = while_rule(&triple1).unwrap();
+        let expected = Triple::new("P", "while B do  done", "∧ ¬ B P");
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_while_rule_different_invariant() {
+        let triple1 = Triple::new("∧ P B", "S", "R"); // Different postcondition
+        let result = while_rule(&triple1);
+        assert!(result.is_err());
+        assert_eq!(result.err().unwrap(), "The loop invariant is not preserved\nprecondition (P∧B): \"P\", postcondition (P): \"R\"".to_string());
+    }
+
+    #[test]
+    fn test_while_rule_complex_invariant() {
+        let triple1 = Triple::new("∧ ∧ A B C", "S", "∧ A B"); // Valid case with complex invariant
+        let result = while_rule(&triple1).unwrap();
+        let expected = Triple::new("∧ A B", "while C do S done", "∧ ¬ C ∧ A B");
+        assert_eq!(result, expected);
+    }
 }
